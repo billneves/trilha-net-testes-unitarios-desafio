0a2854bc1ad9158c65cffcaed379f87bbe90b419
billneves <79235846+billneves@users.noreply.github.com>
2023-12-01 10:56:40 -0300

    Create ValidacoesString.cs

---
+++ projeto resolução/console/ValidacoesString.cs
@@ -0,0 +1,20 @@
+namespace TestesUnitarios.Desafio.Console.Services
+{
+    public class ValidacoesString
+    {
+        public int RetornarQuantidadeCaracteres(string texto)
+        {
+            return texto.Length;
+        }
+
+        public bool ContemCaractere(string texto, string textoProcurado)
+        {
+            return texto.Contains(textoProcurado);
+        }
+
+        public bool TextoTerminaCom(string texto, string textoProcurado)
+        {
+            return texto.EndsWith(textoProcurado);
+        }
+    }
+}
